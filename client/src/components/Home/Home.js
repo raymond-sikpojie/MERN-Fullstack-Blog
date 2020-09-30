1dda4603f0f69f32f314b8b3dea13d018a74ef96
raymond <raymondomon@yahoo.com>
2020-09-30 20:09:53 +0100

    loader css settings changed

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -11,35 +11,37 @@ export default function Home() {
   }, []);
   return (
     <React.Fragment>
-      <div className="hero">
-        <div className="hero-div">
-          <div className="logo-div">
-            {/* <div className="logo">Logo Here</div> */}
-            <Link to="/login">Sign In</Link>
-          </div>
-          <div className="hero-container" data-aos="fade-down">
-            <div className="description">
-              <h1 className="hero-title">Daily Journal</h1>
-              <p>
-                Document your fondest memories. Write about things you love.
-                Talk about those cool stuff that you are most passionate about.
-                Share your ideas with the world. Ramble on about your exciting,
-                new ideas.
-              </p>
-              <Link to="/login">Get Started</Link>
+      <div className="home-main">
+        <div className="hero">
+          <div className="hero-div">
+            <div className="logo-div">
+              {/* <div className="logo">Logo Here</div> */}
+              <Link to="/login">Sign In</Link>
             </div>
-            <div className="image floating" data-aos="fade-left">
-              <img
-                src="https://res.cloudinary.com/ds57wvvno/image/upload/v1600891158/hero_ghs3pe.svg"
-                alt="hero-img"
-              />
+            <div className="hero-container" data-aos="fade-down">
+              <div className="description">
+                <h1 className="hero-title">Daily Journal</h1>
+                <p>
+                  Document your fondest memories. Write about things you love.
+                  Talk about those cool stuff that you are most passionate
+                  about. Share your ideas with the world. Ramble on about your
+                  exciting, new ideas.
+                </p>
+                <Link to="/login">Get Started</Link>
+              </div>
+              <div className="image floating" data-aos="fade-left">
+                <img
+                  src="https://res.cloudinary.com/ds57wvvno/image/upload/v1600891158/hero_ghs3pe.svg"
+                  alt="hero-img"
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="posts">
-        <div>
-          <Posts />
+        <div className="posts">
+          <div>
+            <Posts />
+          </div>
         </div>
       </div>
     </React.Fragment>
