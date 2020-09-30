7883ab96e4804c36c66f7250b9541844b805979e
raymond <raymondomon@yahoo.com>
2020-09-30 15:44:43 +0100

    heroku deploy

---
+++ client/build/precache-manifest.6e1b0516a0c42f4d58554a328c08dd30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c380e8fc36f1e6adfa335645207bf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eee4265d3572f9e27407",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "cac7260af3eac16e47b4",
+    "url": "/static/css/main.b8bb3969.chunk.css"
+  },
+  {
+    "revision": "eee4265d3572f9e27407",
+    "url": "/static/js/2.693c1700.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.693c1700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cac7260af3eac16e47b4",
+    "url": "/static/js/main.ff85f5aa.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- server/server.js
+++ server/server.js
@@ -19,14 +19,14 @@ app.use("/user", userRouter);
 app.use("/comment", commentRouter);
 
 // Heroku config. Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  // set static folder
-  app.use(express.static(path.join(__dirname, "..", "client", "build")));
+// if (process.env.NODE_ENV === "production") {
+// set static folder
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+});
+// }
 
 // set up server
 const PORT = process.env.PORT || 5000;
